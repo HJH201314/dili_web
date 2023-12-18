7c0ec120a6a3a99cb90c9eb45924504112e4fcda
savior <1270084418@qq.com>
2023-12-18 08:07:43 +0800

    feat Iterative4

--- src/components/header/HistorySpinner.vue
+++ src/components/header/HistorySpinner.vue
@@ -1,18 +1,32 @@
 <template>
-  <el-container style="background-color: transparent; margin: 0;">
+  <el-container style="background-color: transparent;">
     <el-main>
-      <div style="width: 400px;background-color: white;border-radius: .5rem; padding: .5rem">
+      <div style="width: 370px;height: 480px;background-color: white;border-radius: .5rem;">
         <el-scrollbar class="scrollbar-wrapper">
           <div v-for="(item, index) in historyList" :key="index" class="history-item" style="display: flex;">
-            <div style="flex: 1; position: relative;width: 182px; height: 5.5rem;">
-              <el-image style="border-radius: 8px; width: 182px; height: 5.5rem;" :src="item.coverUrl"/>
-              <el-text size="small" style="color: black; position: absolute; bottom: 0;
-                 right: 0; background-color: rgba(166, 166, 166, 0.8);
-                  padding: 2px 4px; margin-bottom: 4px; margin-right: 12px; border-radius: 4px;">
-                {{ item.watchedAt }}
-              </el-text>
+            <div style="position: relative;margin-right: 10px; width: 128px; height: 72px;">
+              <el-image style="border-radius: 8px; width: 128px; height: 72px;" :src="item.coverUrl"/>
+              <div style="position: absolute;bottom: 4px;right: 4px;padding: 0;
+              background-color: rgba(0,0,0,.4);border-radius: 2px;line-height: 17px">
+                <span style="display: inline-block;color: white;font-size: 12px;line-height: 14px;transform: scale(.85);transform-origin: center top">
+                {{ item.watchedAt +'/'+ item.duration }}
+                </span>
+              </div>
             </div>
-            <el-space direction="vertical" style="flex: 1;">
+            <div style="display: flex;flex: 1 auto;flex-direction: column;justify-content: space-between">
+              <div title="item.title" style="height: 36px;margin-bottom: 4px;color: #18191c;font-size: 14px;line-height: 18px" >
+                {{item.title}}
+              </div>
+              <div style="display: flex;align-items: center;color: #9499a0;font-size: 12px;line-height: 16px">
+                <el-icon><VideoCamera style="height: 1rem; width: 1rem;" /></el-icon>
+                <span style="margin-right: 8px;">{{item.hisTime}}</span>
+              </div>
+              <div title="item.upName" style="display: flex;align-items: center;color: #9499a0;font-size: 12px;line-height: 16px">
+                <el-icon><User style="height: 1rem; width: 1rem;" /></el-icon>
+                <span style="margin-right: 8px;">{{item.upName}}</span>
+              </div>
+            </div>
+<!--            <el-space direction="vertical" style="flex: 1;">
               <el-text line-clamp="2" size="large" style="color: black; margin-left: 0;">
                 {{ item.title }}
               </el-text>
@@ -33,7 +47,7 @@
                   </el-text>
                 </el-space>
               </el-row>
-            </el-space>
+            </el-space>-->
           </div>
 
         </el-scrollbar>
@@ -42,73 +56,51 @@
   </el-container>
 </template>
 
-<script>
-import { ElImage, ElMain, ElSpace, ElText, ElRow, ElIcon, ElScrollbar, ElContainer } from "element-plus";
-export default {
-  data() {
-    return {
-      historyList: [],
-    };
-  },
-  methods: {
-    getHistoryList() {
-      //todo: 根据用户id请求历史记录
-      return [
-        {
-          uid: 1,
-          watchedAt: "2:30",
-          hisTime: "2021-01-01 12:00:00",
-          title: "视频的标题",
-          upName: "up主的名字",
-          coverUrl: "https://i1.hdslb.com/bfs/archive/99e1ec6c6a6462ea410bd9acc0eebb0eebbc1488.jpg@672w_378h_1c_!web-home-common-cover.webp",
-        },
-        {
-          uid: 2,
-          watchedAt: "2:30",
-          hisTime: "2021-01-01 12:00:00",
-          title: "视频的标题",
-          upName: "up主的名字",
-          coverUrl: "https://i1.hdslb.com/bfs/archive/99e1ec6c6a6462ea410bd9acc0eebb0eebbc1488.jpg@672w_378h_1c_!web-home-common-cover.webp",
-        },
-        {
-          uid: 3,
-          watchedAt: "2:30",
-          hisTime: "2021-01-01 12:00:00",
-          title: "视频的标题",
-          upName: "up主的名字",
-          coverUrl: "https://i1.hdslb.com/bfs/archive/99e1ec6c6a6462ea410bd9acc0eebb0eebbc1488.jpg@672w_378h_1c_!web-home-common-cover.webp",
-        },
-        {
-          uid: 4,
-          watchedAt: "2:30",
-          hisTime: "2021-01-01 12:00:00",
-          title: "视频的标题",
-          upName: "up主的名字",
-          coverUrl: "https://i1.hdslb.com/bfs/archive/99e1ec6c6a6462ea410bd9acc0eebb0eebbc1488.jpg@672w_378h_1c_!web-home-common-cover.webp",
-        },
-        {
-          uid: 5,
-          watchedAt: "2:30",
-          hisTime: "2021-01-01 12:00:00",
-          title: "视频的标题",
-          upName: "up主的名字",
-          coverUrl: "https://i1.hdslb.com/bfs/archive/99e1ec6c6a6462ea410bd9acc0eebb0eebbc1488.jpg@672w_378h_1c_!web-home-common-cover.webp",
-        },
-        {
-          uid: 6,
-          watchedAt: "2:30",
-          hisTime: "2021-01-01 12:00:00",
-          title: "视频的标题",
-          upName: "up主的名字",
-          coverUrl: "https://i1.hdslb.com/bfs/archive/99e1ec6c6a6462ea410bd9acc0eebb0eebbc1488.jpg@672w_378h_1c_!web-home-common-cover.webp",
-        },
-      ]
-    }
-  },
-  mounted() {
-    this.historyList = this.getHistoryList();
-  }
+<script setup lang="ts">
+import { ElImage, ElMain, ElIcon, ElScrollbar, ElContainer } from "element-plus";
+import { onMounted, ref } from "vue";
+import services from "@/apis/services";
+import { getRecentHistoryListUsingGet } from "@/apis/services/video-platform-admin/historyController";
+
+onMounted(() => {
+  getHistoryList();
+});
+
+const myuid = ref<number>();
+
+type HistoryItem = {
+  uid?: number;
+  watchedAt?: string;
+  duration?: number;
+  hisTime?: string;
+  title?: string;
+  upName?: string;
+  coverUrl?: string;
 };
+
+const historyList = ref<HistoryItem[]>([]);
+const back_historyList = ref<API.Vo[]>([]);
+const back_duration = ref<number>();
+
+function getHistoryList() {
+  services.adminService.historyController.getRecentHistoryListUsingGet({userId: myuid.value}).then((res) => {
+    back_historyList.value = res.data.data ?? [];
+    for (let i = 0; i < back_historyList.value.length; i++) {
+      services.adminService.videoController.getVideoInfoUsingGet({ id: res.data.data![i].uid ?? 0 }).then((result) => {
+        back_duration.value = result.data.data!.video?.totalTime ?? 0;
+      })
+      historyList.value.push({
+        uid: back_historyList.value[i].uid,
+        watchedAt: back_historyList.value[i].watchedAt,
+        duration: back_duration.value,
+        hisTime: back_historyList.value[i].hisTime,
+        title: back_historyList.value[i].title,
+        upName: back_historyList.value[i].upName,
+        coverUrl: back_historyList.value[i].coverUrl,
+      });
+    }
+  });
+}
 </script>
 
 <style scoped lang="scss">
@@ -118,6 +110,9 @@ export default {
   overflow: auto;
 }
 .history-item {
+  width: 366px;
+  height: 92px;
+  padding: 10px 20px;
   @extend %hover-able;
   @extend %button-like;
   @extend %transition-all-circ;
--- src/pages/common/home/HomePage.vue
+++ src/pages/common/home/HomePage.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, reactive, ref } from "vue";
+import { onMounted, onUnmounted, reactive, ref } from "vue";
 import CommonHeader from "@/components/header/CommonHeader.vue";
 import { useRouter } from "vue-router";
 import VideoCard from "@/components/video-card/VideoCard.vue";
@@ -8,6 +8,7 @@ import { allPartitionsUsingGet } from "@/apis/services/video-platform-admin/upda
 import DiliButton from "@/components/button/DiliButton.vue";
 import usePartitionStore from "@/stores/usePartitionStore";
 import useUserStore from "@/stores/useUserStore";
+import App from "@/App.vue";
 
 onMounted(() => {
   iconImage.value = document.getElementById("iconImage") as HTMLImageElement;
@@ -16,6 +17,23 @@ onMounted(() => {
   getslides();
   getFeed();
 })
+
+const loading = ref(false);
+const scrollContainer = ref();
+
+function handleScroll() {
+  const scrollTop = scrollContainer.value.scrollTop;
+  const scrollHeight = scrollContainer.value.scrollHeight;
+  const clientHeight = scrollContainer.value.clientHeight;
+  console.log(1);
+  if (scrollTop + clientHeight >= scrollHeight) {
+    loading.value = true;
+    setTimeout(() => {
+      getFeed(); // 假设 getFeed 是一个加载更多数据的方法
+      loading.value = false;
+    }, 1000);
+  }
+}
 
 const router = useRouter();
 const currentSlideIndex = ref(0);
@@ -73,47 +91,13 @@ type Feed = {
 }
 
 const leftCategories = ref<Category[]>([]);
-const rightCategories = ref<Category[]>([
-  {
-    id: 1,
-    name: "动画",
-    link: "/anime",
-  },
-  {
-    id: 2,
-    name: "番剧",
-    link: "/anime",
-  },
-  {
-    id: 3,
-    name: "国创",
-    link: "/anime",
-  },
-  {
-    id: 4,
-    name: "音乐",
-    link: "/anime",
-  },
-  {
-    id: 5,
-    name: "舞蹈",
-    link: "/anime",
-  },
-  {
-    id: 6,
-    name: "游戏",
-    link: "/anime",
-  }
-]);
 
 const video = ref<API.Vo4[]>([]);
 const feednum = ref<API.getHomePageUsingGETParams>({pageSize: 11, uid: 0});
 const feeds = ref<Feed[]>([]);
 const slides = ref<Feed[]>([]);
-const partition = ref<API.Partition[]>([]);
-
 const partitionStore = usePartitionStore();
-const userStore = useUserStore();
+
 function getCategories() {
   leftCategories.value = partitionStore.partition.map((p) => {
     return {
@@ -125,21 +109,34 @@ function getCategories() {
 }
 
 function getslides() {
-  services.adminService.updatesController.getHomePageUsingGet({pageSize: 5, uid: /*userStore.userInfo.id ??*/ 0}).then((res) => {
+  services.adminService.updatesController.getHomePageUsingGet({
+    pageSize: 5,
+    uid: /*userStore.userInfo.id ??*/ 0
+  }).then((res) => {
     video.value = res.data.data ?? [];
     for (let i = 0; i < video.value.length; i++) {
       slides.value.push({
+        id: video.value[i].id!,
         title: video.value[i].title,
-        coverSrc: 'https://cdn.fcraft.cn/image/dilidili/cover.jpg'
-        //coverSrc: video.value[i].url,
+        coverSrc: video.value[i].url,
+        author: video.value[i].upName!,
+        dmnum: video.value[i].dmNum,
+        playnum: video.value[i].playNum,
+        duration: video.value[i].totalTime,
+        publishTime: video.value[i].uploadTime!,
+        url: "/video/id",
       });
     }
   });
 }
 
 function getFeed() {
+/*  if (loading.value) {
+    return;
+  }
+  loading.value = true;*/
   services.adminService.updatesController.getHomePageUsingGet({
-    pageSize: feednum.value.pageSize!,
+    pageSize: 31,
     uid: /*userStore.userInfo.id ??*/ 0,
   }).then((res) => {
     video.value = res.data.data ?? [];
@@ -156,6 +153,7 @@ function getFeed() {
         url: "/video/id",
       })
     }
+/*    loading.value = false;*/
   });
 }
 </script>
@@ -201,8 +199,29 @@ function getFeed() {
         </div>
         <div class="vertical-line"></div>
         <div class="container-item-right">
-          <div v-for="category in rightCategories" :key="category.id" class="channel-link">
-            <a class="a" :href="category.link" target="_blank">{{ category.name }}</a>
+          <div class="rchannel-link">
+            <svg t="1642646834655" class="side-icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4061" width="200" height="200"><path d="M810.6496 153.6C843.6736 153.6 870.4 180.3264 870.4 213.3504v597.2992c0 33.024-26.7264 59.7504-59.7504 59.7504H213.3504A59.7504 59.7504 0 0 1 153.6 810.6496V213.3504C153.6 180.3264 180.3264 153.6 213.3504 153.6z m-332.8 460.8H341.3504a34.1504 34.1504 0 0 0 0 68.2496h136.4992a34.1504 34.1504 0 1 0 0-68.2496z m204.8-136.6016H341.3504a34.1504 34.1504 0 0 0 0 68.2496h341.2992a34.1504 34.1504 0 0 0 0-68.2496z m0-136.3968H341.3504a34.1504 34.1504 0 0 0 0 68.2496h341.2992a34.1504 34.1504 0 1 0 0-68.2496z" p-id="4062"></path></svg>
+            <a class="a" link="/anime" target="_blank">专栏</a>
+          </div>
+          <div class="rchannel-link">
+            <svg t="1642646844599" class="side-icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4188" width="200" height="200"><path d="M772.4544 229.2736l2.048 6.4512 87.2448 283.136c8.8576 33.1264-10.24 66.56-41.984 78.4384l-6.5536 2.048-118.1696 31.744a32.768 32.768 0 0 1-8.192 1.0752l-4.096-0.2048-124.672-14.6432-193.3824 51.8144 36.6592 136.704a49.152 49.152 0 0 1-93.184 30.8736l-1.792-5.4272L155.2896 267.264a49.152 49.152 0 0 1 93.184-30.8736l1.792 5.4272 4.096 15.36 188.928-50.6368a32.768 32.768 0 0 1 8.1408-1.0752l4.096 0.2048 124.5184 14.592 112.1792-30.0544c33.4336-8.96 68.1984 7.7824 80.2304 39.0144z" p-id="4189"></path></svg>
+            <a class="a" link="/anime" target="_blank">活动</a>
+          </div>
+          <div class="rchannel-link">
+            <svg t="1642670011510" class="side-icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3116" width="200" height="200"><path d="M836.3008 170.6496c17.1008 0 31.2832 12.6464 33.7408 29.0816L870.4 204.8v316.16a34.1504 34.1504 0 0 1-29.0816 33.792l-5.0176 0.3584h-102.4v195.7888a34.1504 34.1504 0 0 1-29.1328 33.792l-5.0176 0.3584H386.7648l-127.3344 95.5392a34.2016 34.2016 0 0 1-53.248-17.3568l-1.024-4.8128-0.3584-5.12-0.0512-68.3008H153.6a34.1504 34.1504 0 0 1-32.6656-24.2176l-1.0752-4.8128-0.4096-5.0688v-409.6c0-17.152 12.6464-31.3344 29.0816-33.792L153.6 307.2h174.2336V204.8c0-17.152 12.6464-31.3344 29.1328-33.792l5.0176-0.3584h474.3168z m-477.9008 409.6H256l-5.0688 0.4096a34.1504 34.1504 0 0 0 0 67.4816l5.0688 0.4096h102.4l5.0688-0.4096A34.1504 34.1504 0 0 0 358.4 580.2496z m102.4-136.4992H256l-5.0688 0.4096a34.1504 34.1504 0 0 0 0 67.4816l5.0688 0.4096h204.8l5.0688-0.4096A34.1504 34.1504 0 0 0 460.8 443.7504z m341.2992-204.8512h-406.016v68.2496h303.616c17.152 0 31.3856 12.6464 33.792 29.1328l0.3584 5.0176v145.4592l68.2496 0.0512V238.8992z" p-id="3117"></path></svg>
+            <a class="a" link="/anime" target="_blank">社区</a>
+          </div>
+          <div class="rchannel-link">
+            <svg t="1642646710703" class="side-icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3158" width="200" height="200"><path d="M682.6496 204.8512c35.4816 0 64.6656 27.136 67.9424 61.696l0.3072 6.6048V358.4l81.92-61.44a34.2016 34.2016 0 0 1 53.248 17.408l1.024 4.7616 0.3584 5.12v392.5504a34.1504 34.1504 0 0 1-50.2784 30.1056l-4.352-2.7648-81.92-61.4912v68.3008c0 33.1264-23.552 60.7232-54.8352 66.9696l-6.8608 1.024-6.5536 0.256H204.8a68.2496 68.2496 0 0 1-67.9936-61.696l-0.3072-6.5536V273.152c0-35.4816 27.136-64.6656 61.696-67.9936L204.8 204.8512h477.8496zM385.3824 389.12a34.1504 34.1504 0 0 0-34.0992 34.1504v179.6096a34.1504 34.1504 0 0 0 53.0432 28.416l134.7584-89.8048a34.1504 34.1504 0 0 0 0-56.832L404.3264 394.8544a34.1504 34.1504 0 0 0-18.944-5.7344z" p-id="3159"></path></svg>
+            <a class="a" link="/anime" target="_blank">直播</a>
+          </div>
+          <div class="rchannel-link">
+            <svg t="1642669966795" class="side-icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2862" width="200" height="200"><path d="M749.2096 168.96a102.4 102.4 0 0 1 102.2464 96.3584l0.1536 6.0416v477.8496a102.4 102.4 0 0 1-96.3584 102.2464l-6.0416 0.1536H271.36a102.4 102.4 0 0 1-102.2464-96.3584l-0.1536-6.0416V271.36a102.4 102.4 0 0 1 96.3584-102.2464L271.36 168.96h477.8496z m-69.9392 385.4848a34.1504 34.1504 0 0 0-48.2816 0 170.6496 170.6496 0 0 1-241.3568 0 34.1504 34.1504 0 1 0-48.2816 48.2304 238.9504 238.9504 0 0 0 337.92 0 34.1504 34.1504 0 0 0 0-48.2304zM322.56 339.6608a51.2 51.2 0 0 0-51.2 51.2v34.1504a51.2 51.2 0 1 0 102.4 0v-34.1504a51.2 51.2 0 0 0-51.2-51.2z m375.4496 0a51.2 51.2 0 0 0-51.2 51.2v34.1504a51.2 51.2 0 1 0 102.4 0v-34.1504a51.2 51.2 0 0 0-51.2-51.2z" p-id="2863"></path></svg>
+            <a class="a" link="/anime" target="_blank">课堂</a>
+          </div>
+          <div class="rchannel-link">
+            <svg t="1642669999866" class="side-icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2989" width="200" height="200"><path d="M783.36 221.8496a102.4 102.4 0 0 1 102.0928 94.7712l0.3072 7.68v409.6a102.4 102.4 0 0 1-94.72 102.0928l-7.68 0.256H237.2096a102.4 102.4 0 0 1-102.0928-94.72l-0.3072-7.68v-409.6A102.4 102.4 0 0 1 229.5808 222.208l7.68-0.3072H783.36z m-155.5968 81.408a34.1504 34.1504 0 0 0-50.7904 23.6032l-54.1696 203.3152a128 128 0 1 0-108.288 220.8256l6.7584 1.024a128 128 0 0 0 141.5168-104.8576l71.7824-253.7984 47.4624 35.4816 3.2768 2.2016a34.1504 34.1504 0 0 0 37.5808-56.832l-91.8528-68.7616z" p-id="2990"></path></svg>
+            <a class="a" link="/anime" target="_blank">热榜</a>
           </div>
         </div>
       </div>
@@ -213,7 +232,7 @@ function getFeed() {
       <div class="feed-container">
         <el-carousel class="recommended-swiper" indicator-position="none">
           <el-carousel-item style="height: auto" v-for="(slide, index) in slides" :key="index">
-            <img :src="slide.coverSrc" alt="Slide Image" style="width: 100%;height: auto;">
+            <img :src="'https://api.likepoems.com/img/pc/'" alt="Slide Image" style="width: 100%;height: auto;">
           </el-carousel-item>
         </el-carousel>
         <!--        <div class="recommended-swiper">-->
@@ -240,6 +259,7 @@ function getFeed() {
           :duration="feed.duration"
           auto-fetch
         />
+        <div v-if="loading">Loading...</div>
       </div>
     </div>
   </div>
@@ -358,7 +378,7 @@ function getFeed() {
   display: grid;
   width: 70%;
   grid-template-columns: repeat(9, 1fr);
-  row-gap: 10px;
+  grid-gap: 10px;
   padding-left: 20px;
   padding-right: 10px;
 }
@@ -372,6 +392,8 @@ function getFeed() {
 }
 
 .channel-link {
+  max-width: 95px;
+  width: 100%;
   cursor: pointer;
   margin-right: 10px;
   position: relative;
@@ -383,6 +405,20 @@ function getFeed() {
   border-radius: .5rem;
   text-decoration: none;
 }
+
+.rchannel-link {
+  width: 100%;
+  cursor: pointer;
+  margin-right: 10px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  border-radius: .5rem;
+  text-decoration: none;
+}
+
 
 .channel-link::before {
   content: "";
@@ -399,6 +435,12 @@ function getFeed() {
 .channel-link:hover::before {
   //box-shadow: 0 0 10px rgba(52, 152, 219, 1);
   box-shadow: 0px 0px 5px 0 transparentize($color-primary, 0.1);
+}
+
+.side-icon {
+  width: 20px;
+  height: 20px;
+  margin-right: 5px;
 }
 
 .vertical-line {
